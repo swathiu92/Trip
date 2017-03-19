bb6f87f04eb9bd3e664115b9eaa30c7257fd9de7
swathiu92 <swathiu92@outlook.com>
2017-03-20 01:44:51 +0530

    updated traveler detail screen

--- TTWeb/app-assets/flight/directives/checkout.js
+++ TTWeb/app-assets/flight/directives/checkout.js
@@ -6,10 +6,55 @@
 			$scope.searchmodel = ShareDataService.getSharedData().searchmodel;
 			//$scope.searchmodel.departure = $filter('date')($scope.searchmodel.departure,'MMM dd yyyy');
 			console.log($scope.searchmodel);
+			
 			$scope.travel = ShareDataService.getSharedData().travel;
 			$scope.adultprice = 0;
 			$scope.childprice = 0;
 			$scope.infantprice = 0;
+			$scope.adultInfo = new Array($scope.searchmodel.adult);
+			$scope.chidInfo = new Array($scope.searchmodel.child);
+			$scope.infantInfo = new Array($scope.searchmodel.infant);
+			$scope.firstName = {};
+			$scope.middleName = {};
+			$scope.lastName = {};
+			$scope.travellerInfo = [];
+			for(var a=0;a<$scope.adultInfo.length;a++){
+				var type1 = a+1;
+				$scope.travellerInfo.push({
+					"type":"Adult "+type1 ,
+					"text":"Adult",
+					"firstName":'',
+					"middleName":'',
+					"lastName":'',
+					"title":'',
+					"titleList":[{'name':'Mr.'},{'name':'Mrs.'},{'name':'Ms.'}]				
+				});
+			}
+			for(var a=0;a<$scope.chidInfo.length;a++){
+				var type2 = a+1;
+				$scope.travellerInfo.push({
+					"type":"Child "+type2,
+					"text":"Child",
+					"firstName":'',
+					"middleName":'',
+					"lastName":'',
+					"title":'',
+					"titleList":[{'name':'Master'},{'name':'Miss'}]					
+				});
+			}
+			for(var a=0;a<$scope.infantInfo.length;a++){
+				var type3 = a+1;
+				$scope.travellerInfo.push({
+					"type":"Infant "+type3,
+					"text":"Infant",
+					"firstName":'',
+					"middleName":'',
+					"lastName":'',
+					"title":'',
+					"titleList":[{'name':'Master'},{'name':'Miss'}]	
+				});
+			}
+			
 			console.log($scope.searchmodel.bookDetails);
 			angular.forEach($scope.searchmodel.bookDetails, function(value, key){
 				value.adultprice = (value.adultprice)?(value.adultprice):0;
@@ -20,9 +65,7 @@
 				($scope.searchmodel.infant !== 0)?($scope.infantprice = $scope.infantprice + parseInt(value.infantprice) * $scope.searchmodel.infant):'';
 			});
 			$scope.searchmodel.totalPrice = $scope.adultprice + $scope.childprice + $scope.infantprice;
-			$scope.promo = function(){
-				$scope.selectPromo = !$scope.selectPromo;
-			};
+			 
 			$scope.nextPage = false;
 			$scope.continueToNextPage = function(){
 				/* $scope.nextPage = true; */
@@ -38,7 +81,19 @@
 				$scope.searchmodel.showContainer = 'review';
 			};
 			$scope.goToSummary = function(){
-				$scope.searchmodel.showContainer = 'summary';
+				var moveNext = true;
+				if($scope.travel.contact && $scope.travel.code){
+					for(var c=0;c<$scope.travellerInfo.length;c++){
+						if(!$scope.travellerInfo[c].firstName || !$scope.travellerInfo[c].middleName || !$scope.travellerInfo[c].lastName || !$scope.travellerInfo[c].title){
+							moveNext = false;
+							break;
+						}
+					}
+					if(moveNext){
+						$scope.searchmodel.showContainer = 'summary';	
+					}					
+				}
+				
 			};
 			$scope.backTosearchmodel = function(){
 				$scope.searchmodel.showContainer = 'travellerDetails';
